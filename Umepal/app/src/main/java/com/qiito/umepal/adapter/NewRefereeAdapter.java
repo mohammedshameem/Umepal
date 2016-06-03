a6ac9755668ab6d2fc0d5b3f460355656cc11adf
mohammedshameem <shameem@xminds.in>
2016-06-03 17:01:17 +0530

    new referee

---
+++ Umepal/app/src/main/java/com/qiito/umepal/adapter/NewRefereeAdapter.java
@@ -0,0 +1,110 @@
+package com.qiito.umepal.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.qiito.umepal.R;
+import com.qiito.umepal.Utilvalidate.UtilValidate;
+import com.qiito.umepal.holder.UserObjectHolder;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by vivek on 2/6/16.
+ */
+public class NewRefereeAdapter extends BaseAdapter {
+   // NewRefereeFragment newRefereeFragmentoObject=new NewRefereeFragment();
+    private Activity activity;
+    private LayoutInflater inflater;
+    private ViewHolder viewHolder;
+    private List<UserObjectHolder> newRefereeList;
+
+    public NewRefereeAdapter(Activity activity, List<UserObjectHolder> newRefereeList) {
+        //this.newRefereeFragmentoObject = newRefereeFragmentoObject;
+        this.activity = activity;
+        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        this.newRefereeList=newRefereeList;
+    }
+
+    @Override
+    public int getCount() {
+        return newRefereeList.size() ;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView==null){
+            viewHolder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.new_reffree_listitem, null);
+            viewHolder.pic= (ImageView) convertView.findViewById(R.id.pic);
+            viewHolder.name= (TextView) convertView.findViewById(R.id.name);
+            viewHolder.confirmButton= (Button) convertView.findViewById(R.id.confirmButton);
+            convertView.setTag(viewHolder);
+
+
+        }
+     else {
+        viewHolder = (ViewHolder) convertView.getTag();
+    }
+
+        if (UtilValidate.isNotNull(newRefereeList.get(position).getProfilePic())){
+            if(!newRefereeList.get(position).getProfilePic().equals(""))
+            {
+                Picasso.with(activity)
+                        .load(newRefereeList.get(position).getProfilePic())
+                        .placeholder(R.drawable.logo_splash)
+                        .error(R.drawable.logo_splash)
+                        .into(viewHolder.pic);
+            }
+
+        }
+
+        if (UtilValidate.isNotNull(newRefereeList.get(position).getFirstName())) {
+            viewHolder.name.setText(newRefereeList.get(position).getFirstName());
+        }
+        //lastName
+      /* if (UtilValidate.isNotNull(newRefereeList.get(position).getLastName())){
+            viewHolder.name.setText(newRefereeList.get(position).getLastName());
+        }*/
+        if(UtilValidate.isNotNull(newRefereeList.get(position).getPaymentStatus())){
+            String paymentStatus=newRefereeList.get(position).getPaymentStatus();
+            if (paymentStatus!=null){
+                Log.d("PAYMENT STATUS",paymentStatus );
+                viewHolder.confirmButton.setVisibility(View.VISIBLE);
+            }
+            else{
+                viewHolder.confirmButton.setVisibility(View.INVISIBLE);
+            }
+
+
+        }
+
+        return convertView;
+    }
+    private class ViewHolder {
+        ImageView pic;
+        TextView name;
+        Button confirmButton;
+
+    }
+}
--- Umepal/app/src/main/java/com/qiito/umepal/fragments/NewRefereeFragment.java
+++ Umepal/app/src/main/java/com/qiito/umepal/fragments/NewRefereeFragment.java
@@ -4,15 +4,15 @@ package com.qiito.umepal.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.qiito.umepal.R;
+import com.qiito.umepal.adapter.NewRefereeAdapter;
 import com.qiito.umepal.holder.ListRefereeBaseHolder;
-import com.qiito.umepal.holder.UserBaseHolder;
 import com.qiito.umepal.managers.DbManager;
 import com.qiito.umepal.managers.UserManager;
 import com.qiito.umepal.webservice.AsyncTaskCallBack;
@@ -26,6 +26,8 @@ public class NewRefereeFragment extends Fragment {
     private ListRefereesCallBack listRefereesCallBack;
     private String session;
     private ListRefereeBaseHolder listRefereeBaseHolder;
+
+    ListView newRefList;
 
 
     public NewRefereeFragment() {
@@ -42,9 +44,12 @@ public class NewRefereeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         content = inflater.inflate(R.layout.new_referee_page, container, false);
+        initView();
+
         listRefereesCallBack=new ListRefereesCallBack();
         listRefereeBaseHolder = new ListRefereeBaseHolder();
         session = DbManager.getInstance().getCurrentUserDetails().getSession_id();
+
 
         UserManager.getInstance().ListReferees(getActivity(),session,listRefereesCallBack);
 
@@ -53,13 +58,27 @@ public class NewRefereeFragment extends Fragment {
 
     }
 
-    public class ListRefereesCallBack implements AsyncTaskCallBack{
+    private void initView() {
+        newRefList= (ListView) content.findViewById(R.id.list_view_my_likes);
+
+    }
+
+    public class ListRefereesCallBack implements AsyncTaskCallBack {
 
         @Override
         public void onFinish(int responseCode, Object result) {
             listRefereeBaseHolder = (ListRefereeBaseHolder)result;
 
+
             if(listRefereeBaseHolder.getStatus().equalsIgnoreCase("success")){
+                if(result!=null) {
+                    newRefList.setAdapter(new NewRefereeAdapter(getActivity(),listRefereeBaseHolder.getData()));
+                }
+                else {
+                    Toast.makeText(getActivity(), "There is No Data to show", Toast.LENGTH_SHORT).show();
+                }
+
+
                 Toast.makeText(getActivity(),"success",Toast.LENGTH_SHORT).show();
             }else {
                 Toast.makeText(getActivity(),"fail",Toast.LENGTH_SHORT).show();
